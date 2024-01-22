f24d512b718c4592501cc4b25fcf7aef06d0f8e9
Brice Friha <Brice.friha@outlook.com>
2024-01-22 22:01:51 +0000

    Navigation to the game page

--- Wordster/AppShell.xaml.cs
+++ Wordster/AppShell.xaml.cs
@@ -1,10 +1,14 @@
-﻿namespace Wordster
+﻿using Wordster.Views;
+
+namespace Wordster
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+
+            Routing.RegisterRoute("gamePage", typeof(GamePage));
         }
     }
 }
---
+++ Wordster/ViewModels/GameViewModel.cs
@@ -0,0 +1,16 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wordster.ViewModels
+{
+    class GameViewModel : BaseViewModel
+    {
+        public GameViewModel()
+        {
+        }
+    }
+}
