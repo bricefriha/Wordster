6ee6f3414d4bae749eba678836f50dd30374a479
Brice Friha <Brice.friha@outlook.com>
2024-01-24 00:00:26 +0000

    Add models slots

---
+++ Wordster/Models/Slot.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wordster.Models
+{
+    public class Slot : NotifiableModel
+    {
+        private ObservableCollection<string> _letters;
+
+        public ObservableCollection<string> Letters 
+        {
+            get
+            {
+                return _letters;
+            }
+            set
+            {
+                _letters = value;
+                OnPropertyChanged(nameof(Letters));
+
+            } 
+        }
+    }
+}
--- Wordster/ViewModels/GameViewModel.cs
+++ Wordster/ViewModels/GameViewModel.cs
@@ -1,16 +1,42 @@
 ﻿using MvvmHelpers;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Wordster.Models;
 
 namespace Wordster.ViewModels
 {
     class GameViewModel : BaseViewModel
     {
+        private const int characterCountMax = 5;
+        private ObservableCollection<Slot> _slots;
+
+        public ObservableCollection<Slot> Slots
+        {
+            get { return _slots; }
+            set 
+            {
+                _slots = value;
+                OnPropertyChanged(nameof(Slots));
+            }
+        }
+
         public GameViewModel()
         {
+            _slots = new ObservableCollection<Slot>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Slot slot = new Slot();
+                slot.Letters = new ObservableCollection<string>();
+                for (int si = 0; si < characterCountMax; si++)
+                    slot.Letters.Add("");
+
+                Slots.Add(slot);
+            }
         }
     }
 }
