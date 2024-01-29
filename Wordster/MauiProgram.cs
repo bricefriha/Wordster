0816bfb644acc893549469af171c021b858b224f
Brice Friha <Brice.friha@outlook.com>
2024-01-29 22:23:29 +0000

    Sort out dependancy mess

--- Wordster/MauiProgram.cs
+++ Wordster/MauiProgram.cs
@@ -1,6 +1,6 @@
-﻿
-using Maui.NeoControls;
+﻿using Maui.NeoControls;
 using Microsoft.Extensions.Logging;
+using CommunityToolkit.Maui;
 
 namespace Wordster
 {
@@ -9,22 +9,17 @@ namespace Wordster
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .UseNeoControls()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                    fonts.AddFont("ComicShark.otf", "ComicShark");
-                    fonts.AddFont("SonicComics.ttf", "SonicComics");
-                    fonts.AddFont("MouseMemoirs-Regular.ttf", "MouseMemoirs-Regular");
-                });
-
+            builder.UseMauiApp<App>().UseNeoControls().ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("ComicShark.otf", "ComicShark");
+                fonts.AddFont("SonicComics.ttf", "SonicComics");
+                fonts.AddFont("MouseMemoirs-Regular.ttf", "MouseMemoirs-Regular");
+            }).UseMauiCommunityToolkit();
 #if DEBUG
-		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
-
             return builder.Build();
         }
     }
