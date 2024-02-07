06d5f00d83394fa1880c958e8e2f8d7c3c861b62
Brice Friha <Brice.friha@outlook.com>
2024-02-07 20:55:18 +0000

    Word generation - method to generate a word

--- Wordster/Services/Fetcher.cs
+++ Wordster/Services/Fetcher.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CustardApi.Objects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,24 @@ namespace Wordster.Services
 {
     public class Fetcher
     {
-        
+        private readonly Service _wsHerokuApp;
+
         public Fetcher() 
         {
+            _wsHerokuApp = new Service("random-word-api.herokuapp.com", sslCertificate: true) ;
+        }
+        /// <summary>
+        /// Returns a random word using parameters given
+        /// </summary>
+        /// <param name="length">length of the word you want to be returned</param>
+        /// <param name="lang">language of the word you want</param>
+        /// <returns></returns>
+        public async Task<string> GetRandomWord(int length, string lang = "en")
+        {
+            // Define
+            string controller = $"word?length={length}&lang={lang}";
+            // Process
+            return (await _wsHerokuApp.Get<string[]>("word"))[0];
 
         }
     }
